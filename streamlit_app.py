75182243809b4972f593aefce4df38ec18c3fbe2
s0n41999 <166301423+s0n41999@users.noreply.github.com>
2024-11-05 14:05:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error  
 import requests
 import feedparser
+from xgboost import XGBRegressor
 
 #-----------------NASTAVENIA-----------------
 
